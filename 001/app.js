3cf976adb869246c13b559f0fdb9d059867d1fbd
tomasbalvocius <tomasbalv@gmail.com>
2023-06-28 13:04:30 +0300

    iguanos

---
+++ 001/app.js
@@ -0,0 +1,42 @@
+console.log('Hello World');
+
+
+// let a = true;
+
+
+// console.log(typeof a, a);
+
+// // a = a + '';
+
+// a = a + 10;
+
+// console.log(typeof a, a);
+
+// let c = 1;
+
+// && ||
+
+
+// if (c-- && c) {
+//     console.log('YES');
+    
+// }
+// else {
+//     console.log('NO');
+// }
+
+
+let result;
+
+let digit = 5;
+
+
+// if (3 > digit) {
+//     result = 'YES';
+// } else {
+//     result = 'NO';
+// }
+
+10 > digit && (result = 'YES');
+
+console.log(result);
